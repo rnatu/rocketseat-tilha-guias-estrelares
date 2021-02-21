e5cb39c87d3f78131818e403efa4bc087a26205c
Renato Xavier <rnatu91@gmail.com>
2021-02-21 12:45:27 -0300

    JS: Number

---
+++ javascript/3-tipos-de-dados/number.js
@@ -0,0 +1,18 @@
+/*
+
+  Number
+    * numeros
+
+  33 // inteiros
+  12.5 // reais
+  NaN // Not a Number
+  Infinity // infinito
+*/
+
+console.log(33);
+console.log(12.5);
+console.log(12.5 / 33);
+console.log(12.5 / 'asdf');
+console.log(12.5 === Infinity);
+
+
