7546e3e115e8859fee53643fe7b318f05c0c9d8c
Renato Xavier <rnatu91@gmail.com>
2021-02-20 21:47:12 -0300

    JS - Strings

---
+++ javascript/3-tipos-de-dados/string.js
@@ -0,0 +1,16 @@
+/*
+String
+
+Cadeia de caracteres
+
+"" // aspas duplas
+'' // aspas simples
+`` // template literals ou template strings
+
+*/
+
+console.log("esse é um texto");
+console.log('esse é um texto');
+console.log(' "esse é um texto" ');
+console.log(` esse é um texto ' ' "" `);
+console.log(` esse é um texto ${7+1}`);
