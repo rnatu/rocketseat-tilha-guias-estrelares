660e9fa08a421526df77512a19b9ede5ffcbe321
Renato Xavier <rnatu91@gmail.com>
2021-02-27 23:41:20 -0300

    JS: boolean

---
+++ javascript/3-tipos-de-dados/boolean.js
@@ -0,0 +1,20 @@
+/*
+  Boolean
+
+    * somente 2 valores
+
+      true // verdadeiro
+      false // falso
+
+
+*/
+
+
+console.log(true)
+console.log(false)
+
+const num = 'valor';
+
+console.log('')
+console.log(typeof num === 'string')
+console.log(typeof num === 'number')
