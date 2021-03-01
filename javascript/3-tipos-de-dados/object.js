62dfd1ae0b642539a3383f5187152b3fc05bfe83
Renato Xavier <rnatu91@gmail.com>
2021-03-01 13:05:34 -0300

    JS: Object

---
+++ javascript/3-tipos-de-dados/object.js
@@ -0,0 +1,17 @@
+/*
+* Objeto
+  * Objeto
+  * Propriedades / Atributos
+  * Funcionalidades / Métodos
+
+  { propriedade: "valor" }
+
+*/
+
+console.log({
+  name: 'Renato',
+  age: 36,
+  andar: function() {
+    console.log('Andar')
+  }
+})
