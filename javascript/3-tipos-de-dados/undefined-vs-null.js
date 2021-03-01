3395a36ff31b309186f36c23527ce25a7e676784
Renato Xavier <rnatu91@gmail.com>
2021-02-28 23:23:05 -0300

    JS - undefined vs null

---
+++ javascript/3-tipos-de-dados/undefined-vs-null.js
@@ -0,0 +1,14 @@
+/*
+  * undefined
+    * indefinido
+
+  * null
+    * nulo
+    * objeto que não possui nada dentro
+    * diferente de indefinido
+*/
+
+const a = null;
+console.log(a === undefined);
+
+console.log(b);
