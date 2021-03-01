3f1ad77d3ac5c24735f0b20f96025633b51a33b2
Renato Xavier <rnatu91@gmail.com>
2021-03-01 13:12:31 -0300

    JS: Array

---
+++ javascript/3-tipos-de-dados/array.js
@@ -0,0 +1,31 @@
+/*
+* Array (Vetores)
+  * Uma lista
+  * Agrupamento de dado
+
+  ["Mayk", 36]
+*/
+const lista = ["Renato", 30, "Osasco"];
+
+const listaObj = [
+  {
+    cor: "Azul",
+    material: "Vidro",
+  },
+  {
+    marca: "sony",
+    tipo: "TV",
+  },
+  {
+    bateria: "Lítio",
+    peso: 2,
+  },
+];
+
+console.log(lista);
+console.log(lista[0]);
+console.log("");
+
+console.log(listaObj);
+console.log(listaObj[0]);
+console.log(listaObj[0].material);
