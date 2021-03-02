abe33561228457f1c47677720692f1f3b2c9b85e
Renato Xavier <rnatu91@gmail.com>
2021-03-02 16:40:42 -0300

    JS: Declaration assignment var

---
+++ javascript/5-praticando-e-avancando/declaration-assignment-var.js
@@ -0,0 +1,9 @@
+// Variáveis e tipos de dados
+// declaração or declaration
+var nome;
+
+//assignment or atribuição de valores
+nome = 'Renato';
+
+// que tipo de dado foi colocado na variável
+console.log(typeof nome);
