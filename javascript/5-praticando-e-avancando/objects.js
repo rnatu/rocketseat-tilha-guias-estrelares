52ca35d981a974071cada225d6c3a40b921e3a7b
Renato Xavier <rnatu91@gmail.com>
2021-03-02 16:58:46 -0300

    JS: Objects

---
+++ javascript/5-praticando-e-avancando/objects.js
@@ -0,0 +1,22 @@
+const person = {
+  name: 'John',
+  age: 21,
+  weight: 88.6,
+  isAdmin: true,
+    address: {
+      country: 'Brasil',
+      city: 'São Paulo'
+    }
+}
+
+person.age = 35;
+person.favoriteColor = 'Vermelho'
+
+console.log(
+  `
+  ${person.name} tem ${person.age} anos e mora em ${person.address.city}
+   sua cor favorita é ${person.favoriteColor}
+  `
+  )
+
+console.log(person)
