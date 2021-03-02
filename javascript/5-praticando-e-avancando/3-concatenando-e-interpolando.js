7bd84c727a2cb7a91ff99574bcd10169026e2e4e
Renato Xavier <rnatu91@gmail.com>
2021-03-02 16:53:31 -0300

    JS: Concatenando e interpolando variáveis

---
+++ javascript/5-praticando-e-avancando/3-concatenando-e-interpolando.js
@@ -0,0 +1,12 @@
+let firstName, age, isHuman;
+
+firstName = 'Renato'
+age = 30;
+isHuman = true;
+
+// escrita de texto + variáveis
+//# concatenando os valores;
+console.log('O ' + firstName + ' tem ' + age + ' anos.')
+
+//# interpolando valores com template literals or template strings
+console.log(`O ${firstName} tem ${age} anos.`)
