758ba6bd8161c284d34b473f5a72cea892c9c950
Renato Xavier <rnatu91@gmail.com>
2021-03-02 16:50:19 -0300

    JS: Agrupando declarações

---
+++ javascript/5-praticando-e-avancando/2-agrupando-declaracoes.js
@@ -0,0 +1,11 @@
+
+// declarações conjuntas
+//! Somente com let
+let firstName, age, isHuman;
+
+firstName = 'Renato'
+age = 30;
+isHuman = true;
+
+console.log(firstName, age, isHuman);
+console.log(typeof firstName, typeof age, typeof isHuman);
