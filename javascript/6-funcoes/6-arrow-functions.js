3de6888515b073f149630ae83cdc9c9ccd288c46
Renato Xavier <rnatu91@gmail.com>
2021-03-03 16:09:50 -0300

    JS: arrow function

---
+++ javascript/6-funcoes/6-arrow-functions.js
@@ -0,0 +1,14 @@
+// arrow function expression
+
+//! não sofre hoisting
+const sayMyName = () => {
+  console.log('Renato');
+}
+sayMyName()
+
+// se houver só uma linha de código dentro da função, poderá ser escrita assim:
+const sayMyLastName = (lastName) => console.log(lastName);
+sayMyLastName('Xavier');
+
+
+
