aef3193aea67797bef5e3a2ce1622300b0c7fffa
Renato Xavier <rnatu91@gmail.com>
2021-03-03 14:19:28 -0300

    JS: Function hoisting

---
+++ javascript/6-funcoes/5-function-hoisting.js
@@ -0,0 +1,16 @@
+// function hoisting
+
+sayMyName();
+// declaration - statement
+//! sofre hoisting
+function sayMyName() {
+  console.log('Renato');
+}
+
+
+sayMyLastName();
+// expression
+//! nao sofre hoisting (mesmo utilizando var);
+var sayMyLastName = function() {
+  console.log('Xavier');
+}
