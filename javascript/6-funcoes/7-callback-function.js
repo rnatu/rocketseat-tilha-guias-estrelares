b64aa0907be64ff7694a93abb92950f78023515b
Renato Xavier <rnatu91@gmail.com>
2021-03-03 16:20:01 -0300

    JS: callback function

---
+++ javascript/6-funcoes/7-callback-function.js
@@ -0,0 +1,20 @@
+//callback function - Receber uma função como parametro, para ser executada na função ou utilizado o retorno
+function sayMyName(callbackfn) {
+  console.log("Antes de executar a função callback");
+
+  callbackfn();
+
+  console.log("Depois de executar a função callback");
+}
+
+sayMyName(() => {
+  console.log("Estou em uma callback, Meu nome é Renato");
+});
+
+
+console.log("");
+const sayMyLastName = (lastName) => lastName;
+
+const entireName = (callback) => console.log(`meu sobrenome é ${callback}`);
+
+entireName(sayMyLastName("Xavier"));
