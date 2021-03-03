512ce6648ed924ad89c224346c236b4814a1fdd2
Renato Xavier <rnatu91@gmail.com>
2021-03-03 12:55:25 -0300

    JS: Retornando valores dentro da função

---
+++ javascript/6-funcoes/3-retornando-valores.js
@@ -0,0 +1,13 @@
+//toda função retorna algo, quando não especificado, retorna undefined
+const sum = function(number1, number2)  {
+  let total = number1 + number2;
+  return total;
+}
+
+let num1 = 550;
+let num2 = 350;
+sum(num1, num2);
+
+console.log(`o número 1 é ${num1}`);
+console.log(`o número 2 é ${num2}`);
+console.log(`a soma é ${sum(num1, num2)}`)
