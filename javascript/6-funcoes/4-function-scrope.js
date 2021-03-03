b98c244cec2b201d02b9d5252f795282e8509de7
Renato Xavier <rnatu91@gmail.com>
2021-03-03 14:09:50 -0300

    JS: Function scope

---
+++ javascript/6-funcoes/4-function-scrope.js
@@ -0,0 +1,19 @@
+// function scope
+
+let subject = 'Create video';
+
+function createThink() {
+  subject = "Study" // manipulando a variável
+  return subject
+}
+
+function createThink2(subject) {
+  subject = "Do excercise" //manipulando o parametro
+  return subject
+}
+
+console.log(createThink());
+
+console.log(createThink2(subject));
+
+console.log(subject)
