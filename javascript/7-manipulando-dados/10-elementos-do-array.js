bdc56fb89257971ba7edb4d272f97c3a93fe3578
Renato Xavier <rnatu91@gmail.com>
2021-03-04 15:44:32 -0300

    JS: Elementos do array

---
+++ javascript/7-manipulando-dados/10-elementos-do-array.js
@@ -0,0 +1,25 @@
+// Manipulando Arrays
+
+// Contar elementos de um array
+
+let myArray = ['a', 2, function() {return 'olá'}, { color: 'red'}];
+console.log(myArray.length);
+
+console.log('Executando a função que está no array ->',myArray[2]())
+
+console.log('Acessando a chave color do objeto dentro do array ->',myArray[3].color)
+
+console.log('')
+
+let myObjArray = [
+  {
+    color: 'red',
+    model: 'redmi'
+  },
+  {
+    color: 'black',
+    model: 'samsung'
+  }
+]
+
+console.log(myObjArray.length);
