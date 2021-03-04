6d5e65ad535e8fa3d0dba0c8e3ba9b33abec55b0
Renato Xavier <rnatu91@gmail.com>
2021-03-04 14:31:16 -0300

    JS: Strings em números

---
+++ javascript/7-manipulando-dados/3-strings-em-numeros.js
@@ -0,0 +1,10 @@
+// Manipulando Strings e Números
+
+// Transformar String em Número e Número em String
+let string = "123";
+console.log(typeof Number(string));
+
+console.log('');
+
+let number = 321;
+console.log(typeof String(string));
