69ce86b138433804eac9cf9da3adff63a03e90d9
Renato Xavier <rnatu91@gmail.com>
2021-03-04 14:22:33 -0300

    JS: Prototype

---
+++ javascript/7-manipulando-dados/prototype.js
@@ -0,0 +1,13 @@
+/*
+  Prototype
+
+  * prototype-base language
+  * prototype chain
+  * __proto__
+
+*/
+
+// faça no navegador para visualizar o __proto__
+'Renato'.__proto__
+23.0.__proto__
+true.__proto__
