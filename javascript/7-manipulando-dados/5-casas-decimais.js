dd3e8b10454eaa876bcc248dc951cbb3222198e3
Renato Xavier <rnatu91@gmail.com>
2021-03-04 14:46:26 -0300

    JS: Casas decimais

---
+++ javascript/7-manipulando-dados/5-casas-decimais.js
@@ -0,0 +1,7 @@
+// Manipulando Strings e Números
+
+// Transformar um número quebrado com 2 casas decimais e trocar ponto por vírgula
+
+let number = 345.33452345;
+//! toFixed() transforma o tipo Number em String;
+console.log(number.toFixed(2).replace('.', ','))
