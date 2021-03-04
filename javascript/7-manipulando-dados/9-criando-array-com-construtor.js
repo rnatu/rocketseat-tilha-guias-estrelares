4e87dd5461c0cce1d7ea62ce71b8f869ffab627d
Renato Xavier <rnatu91@gmail.com>
2021-03-04 15:33:16 -0300

    JS: Criando array com construtor

---
+++ javascript/7-manipulando-dados/9-criando-array-com-construtor.js
@@ -0,0 +1,10 @@
+// Manipulando Arrays
+
+// Criar Array com contrutor
+let myArray = new Array('a', 'b', 'c');
+console.log(myArray);
+
+console.log('')
+
+let myArray2 = new Array(10);
+console.log(myArray2);
