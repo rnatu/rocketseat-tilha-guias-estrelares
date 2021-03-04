62207d748cb585c95d23aab4cd586bc622190e05
Renato Xavier <rnatu91@gmail.com>
2021-03-04 14:28:39 -0300

    JS: Type conversion e Type coersion

---
+++ javascript/7-manipulando-dados/2-type-conversion-coersion.js
@@ -0,0 +1,13 @@
+/*
+
+  Type conversion (typecasting) vs Type coersion
+
+  * Alteração de um tipo de dado para outro tipo
+
+*/
+
+// Type coersion - (feito pelo javascript)
+console.log('9' + 5);
+
+// Type conversion (typecasting) - feito através da nossa tomada de decisão
+console.log(Number('9') + 5);
