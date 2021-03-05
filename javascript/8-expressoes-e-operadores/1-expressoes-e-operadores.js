cdb42db73f86990fb6d66006993ed2bde8f99904
Renato Xavier <rnatu91@gmail.com>
2021-03-05 14:34:36 -0300

    JS - Expressões e operadores

---
+++ javascript/8-expressoes-e-operadores/1-expressoes-e-operadores.js
@@ -0,0 +1,26 @@
+/* Expressões e Operadores
+
+- Expressions
+- Operators
+  Binary
+  Unary
+  Ternary
+*/
+
+//expressão
+let number = 1; //necessário o uso do ; antes de uma função auto-executavel
+
+(function () {
+  console.log('Alo');
+})();
+
+//operadores Binary (utiliza de dois elementos e o operador entre eles)
+console.log(number + 1);
+
+//operadores Unary (utiliza de um elemento)
+console.log(++number);
+console.log(--number);
+console.log(typeof number);
+
+//operadores Ternary
+console.log(true ? 'Oláááá' : 'nada');
