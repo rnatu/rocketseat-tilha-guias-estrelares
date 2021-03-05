164ec6b4cfe79f6a9c84121dce540416f24bb072
Renato Xavier <rnatu91@gmail.com>
2021-03-05 17:21:48 -0300

    JS: new

---
+++ javascript/8-expressoes-e-operadores/2-new.js
@@ -0,0 +1,15 @@
+/*
+  new
+
+  * left-hand-side expression
+  * criar um novo objeto usando as funções construtoras do javascript
+*/
+
+let nome = new String('Renato');
+nome.sobreNome = "Xavier";
+
+let age = new Number(23);
+console.log(nome, age)
+
+let date = new Date('2020-12-01');
+console.log(date);
