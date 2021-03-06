f88ce37d72cd742e598ae0044c6fbd66306cb39f
Renato Xavier <rnatu91@gmail.com>
2021-03-06 19:56:26 -0300

    JS: typeof e delete

---
+++ javascript/8-expressoes-e-operadores/3-typeof-e-delete.js
@@ -0,0 +1,16 @@
+/*
+
+  Operadores unários
+  typeof
+  delete
+
+*/
+console.log(typeof "mayke");
+
+const person = {
+  name: 'Mayk',
+  age: 25,
+}
+
+delete person.age; //se existir deleta a propriedade/chave
+console.log(person);
