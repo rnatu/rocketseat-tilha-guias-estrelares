50b76c2b37fc5e1e00b3aeee68d5a0ed84aaa515
Renato Xavier <rnatu91@gmail.com>
2021-03-07 16:40:04 -0300

    JS: Operadores aritméticos

---
+++ javascript/8-expressoes-e-operadores/4-operadores-aritmeticos.js
@@ -0,0 +1,28 @@
+// Operadores Aritiméticos
+
+// multiplicação *
+console.log('multiplicação', 3.2 * 5.3);
+// divisão /
+console.log('divisão', 10 / 5);
+// soma
+console.log('soma', 10 + 5);
+// subtração
+console.log('subtração', 10 - 5);
+
+// resto da divisão %
+let remainder //resto
+remainder = 11 % 4;
+console.log('remainder',remainder)
+
+// incremento ++
+let increment = 0;
+increment++
+console.log('increment', increment)
+
+// decremento --
+let decrement = 0;
+decrement--
+console.log('increment', decrement)
+
+// exponencial **
+console.log(3 ** 2);
