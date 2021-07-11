02b3876f9e38f1d6253b10a391d656030010de24
Renato Xavier <rnatu91@gmail.com>
2021-07-11 17:11:25 -0300

    Operadores lógicos

---
+++ javascript/8-expressoes-e-operadores/10-operadores-logicos.js
@@ -0,0 +1,15 @@
+// Operadores lógicos (logical operators)
+
+// - 2 valores booleanos, quando verificados, resultará em verdadeiro ou falso
+
+let pao = false;
+let queijo = false;
+
+// AND &&
+if(pao && queijo) console.log('Tem pão E queijo')
+
+// OR ||
+if(pao || queijo) console.log('Tem pão OU queijo')
+
+// NOT !
+if(!pao) console.log('Não tem pão')
