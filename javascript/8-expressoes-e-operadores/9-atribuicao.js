d3843d15e5d1975f7478690af7e470997e68500e
Renato Xavier <rnatu91@gmail.com>
2021-07-11 17:02:17 -0300

    Atribuilções

---
+++ javascript/8-expressoes-e-operadores/9-atribuicao.js
@@ -0,0 +1,26 @@
+// Operadores de atribuição (Assignment)
+let x;
+console.log(x);
+
+//# assignment
+x = 4;
+
+//* addition assignment
+// x += 2; // x + 2
+
+//* subtraction assignment
+// x -= 2; // x - 2
+
+//* multiplication assignment;
+// x *= 2; // x * 2
+
+//* division assignment
+// x /= 2; // 4 / 2
+
+//* remainder
+// x %= 2; // resto da divisão 4 / 2
+
+//* exponentiation
+x **= 2; // 4²
+
+console.log(x);
