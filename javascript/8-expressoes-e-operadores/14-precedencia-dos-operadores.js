208fa8cf704dfd4b81265ff70ef27664c6bfca48
Renato Xavier <rnatu91@gmail.com>
2021-07-13 15:12:32 -0300

    Precedência dos operadores

---
+++ javascript/8-expressoes-e-operadores/14-precedencia-dos-operadores.js
@@ -0,0 +1,14 @@
+/* Operator precedence (Procedência de operadores);
+
+* grouping                    ( )
+* negação e incremento        ! ++ --
+* multiplicação e divisão     * /
+* adição e subtração          + -
+* relacional                  < <= > >=
+* igualdade                   == != === !==
+* AND                         &&
+* OR                          ||
+* condicional                 ?:
+* assignment (atribuição)     = += -= *=
+
+*/
