4326e4f3dd721e033dfd2cc84d50968ce247f90e
Renato Xavier <rnatu91@gmail.com>
2021-07-14 15:00:47 -0300

    Inner scope

--- javascript/4-variaveis/youtube-bonus/6-inner.js
+++ javascript/4-variaveis/youtube-bonus/6-inner.js
@@ -1,1 +1,37 @@
 // https://youtu.be/klgtCYfpYyU?t=1418
+
+//# Inner Scope
+// 1. Um escopo dentro de outro ex.:
+// Função dentro de função ou if dentro de função
+{
+  const message = 'Mensagem nova';
+  {
+    {
+      console.log(message); // Mensagem nova
+    }
+  }
+}
+
+// outro exemplo
+const message = 'Minha message'
+
+function sayHello() {
+  const message = 'Hello World!'
+
+  if(true) {
+    console.log(message)
+  }
+}
+sayHello() // console.log(message) // Hello World!
+
+console.log(message) // Minha message
+
+//! LEMBRANDO QUE O VAR NÃO RESPEITA ESCOPO
+{
+  {
+    {
+      var fullName = 'Renato Xavier';
+    }
+  }
+}
+console.log(fullName); //! 'Renato Xavier'
