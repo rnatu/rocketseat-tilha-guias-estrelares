dd11ff36b27e412f19fdaa78d545d6b1ae99a55b
Renato Xavier <rnatu91@gmail.com>
2021-07-16 14:23:15 -0300

    for

---
+++ javascript/10-estruturas-de-repeticao/for.js
@@ -0,0 +1,14 @@
+//# for
+
+for(let i = 0; i <= 10; i++) {
+  if(i === 3) {
+    continue; //pula a execução do momento
+  }
+  console.log(i)
+  if(i === 8) {
+    break; //para a execução do loop
+  }
+}
+
+
+
