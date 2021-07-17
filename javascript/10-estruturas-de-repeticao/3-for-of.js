5bdcafb28f804c096c9b53ca0464caa9a6516966
Renato Xavier <rnatu91@gmail.com>
2021-07-17 20:32:08 -0300

    for of

---
+++ javascript/10-estruturas-de-repeticao/3-for-of.js
@@ -0,0 +1,19 @@
+// for...of
+
+let name = "Mayk"
+let names = ['João', 'Paulo', 'Pedro'];
+
+for(char of name) {
+  console.log(char);
+}
+
+console.log('');
+
+for(name of names) {
+  console.log(name);
+}
+
+console.log('');
+for(item of ['lapís', 'caneta', 'caderno']) {
+  console.log(item);
+}
